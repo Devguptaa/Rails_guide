4de1e9ec8cc3e29354c49bdd5b308c7c93affc30
Dev Gupta <dev.gupta@reddoorz.com>
2024-03-05 16:17:13 +0530

    Rails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,6 +73,21 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  config.action_controller.raise_on_missing_callback_actions = true
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'guptadevgupta8@gmail.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  user_name: 'guptadevgupta8@gmail.com',
+  password: 'pmkoctoenjlglyuk',
+  authentication:'plain',
+  enable_starttls_auto: true  }
+  # config.action_mailer.default_url_options = {:host =>'localhost:3000'}
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
 
